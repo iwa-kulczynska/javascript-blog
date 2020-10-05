14096d76bc52a3961676d31e2f20378d9abf2f88
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-10-06 00:22:02 +0200

    add articles list

--- js/script.js
+++ js/script.js
@@ -48,3 +48,46 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function clearTitlelist(){
+    document.querySelector(optTitleListSelector).innerHTML = '';
+}
+
+function generateTitleLinks(){
+    const titleList = document.querySelector(optTitleListSelector);
+    console.log(titleList);
+  /* remove contents of titleList */
+
+    clearTitlelist();
+
+  /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = "";
+
+    for(let article of articles){
+        article.addEventListener('click', titleClickHandler);
+        
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+
+        /* find the title element */
+        /* get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML);
+
+        /* insert link into titleList */
+        html = html + linkHTML;
+    }
+    console.log(html);
+    titleList.innerHTML = html;
+}
+
+generateTitleLinks();
